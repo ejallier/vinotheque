5812488555a27b6c82632944b9f40a1e36a290e4
Erwann Jallier <erwann.jallier@gmail.com>
2015-02-26 15:23:34 +0100

    static wip

--- front/index.php
+++ front/index.php
@@ -33,19 +33,89 @@
   </div>
 </nav>
 
-<div class="container">
+
 
 	<header class="big-header text-center">
-		<h1 class="mastoc">Ma Vinothèque</h1>
-		<div class="text-center">
-			<a href="" class="btn btn-default login">Login</a>
+		<div class="container">
+			<h1 class="mastoc">Ma Vinothèque</h1>
+			<div class="text-center">
+				<a href="" class="btn btn-ghost login btn-lg">Login</a>
+			</div>
 		</div>
-
 	</header>
 
 
+<main>
+
+	<div class="container">
+		<div class="page-header">
+			<h1>Mieux classer votre vin
+			<small>Pour mieux le boire</small></h1>
+			<p>Un exercice de PhP procédural</p>
+		</div>
+		<div class="row">
+
+
+			<div class="col-md-6">
+				<article>
+					<h2>Objectifs</h2>
+					<ul>
+						<li>Savoir écrire du PhP procédural</li>
+						<li>Rendre dynamique des pages statiques HTML/CSS grâce à PhP</li>
+						<li>Envoyer un mail par un formulaire</li>
+						<li>CRUDer comme un dieu</li>
+					</ul>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<article>
+					<h2>Lorem</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae ipsa aut maxime debitis eligendi, voluptas sint tempore a non, necessitatibus dolorem labore saepe quia dolores alias sit voluptatibus placeat.</p>
+					<p>Aliquam ab vero adipisci, ratione recusandae iusto placeat iste. Qui earum totam enim perspiciatis corrupti similique laborum ut eaque perferendis magni, mollitia nihil veniam nostrum, ex facere ipsum deserunt tempore.</p>
+				</article>
+
+			</div>
+
+<div class="row">
+	<div class="col-md-3 text-center">
+		<div class="project">
+			<i class="glyphicon glyphicon-glass"></i>
+		</div>
+		<p>Jean-Lou voulait qu'on parle de boisson.<br>Encore</p>
+	</div>
+	<div class="col-md-3 text-center">
+		<div class="project">
+			<i class="glyphicon glyphicon-briefcase"></i>
+		</div>
+		<p>Mais bon, en même temps, 'faut bosser, ...</p>
+	</div>
+	<div class="col-md-3 text-center">
+		<div class="project">
+			<i class="glyphicon glyphicon-education"></i>
+		</div>
+		<p>Et puis aprendre des trucs, aussi</p>
+	</div>
+	<div class="col-md-3 text-center">
+		<div class="project">
+			<i class="glyphicon glyphicon-glass"></i>
+		</div>
+		<p>Mais jean-Lou voulait <em>Vraiment</em> qu'on parle de boisson!</p>
+	</div>
 </div>
 
+		</div>
+	</div>
+</main>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">CEFIM &copy; <?= date(Y) ?></div>
+			<div class="col-md-4"><div class="text-center">Vinothèque</div></div>
+			<div class="col-md-4"><div class="text-right">F. Ormateur</div></div>
+		</div>
+	</div>
+</footer>
 <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </body>
