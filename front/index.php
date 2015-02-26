a4af0828857ddbded0902bbc3820afffb47227b2
Erwann Jallier <erwann.jallier@gmail.com>
2015-02-26 11:48:00 +0100

    home en cours

---
+++ front/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vinothèque</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/yeti/bootstrap.min.css">
+	<link rel="stylesheet" href="../style/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+	<div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-block">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand " href="index.php">Vinothèque</a>
+    </div>
+
+    <div id="nav-block" class="collapse navbar-collapse">
+    	<ul class="nav navbar-nav">
+    		<li><a href=""><i class="glyphicon glyphicon-book glyphicon-lg"></i> Liste des vins</a></li>
+    		<li><a href=""><i class="glyphicon glyphicon-pencil glyphicon-lg"></i> Ajouter un vin</a></li>
+			<li><a href=""><i class="glyphicon glyphicon-search glyphicon-lg"></i> Chercher le bon vin</a></li>
+    	</ul>
+    </div>
+
+
+  </div>
+</nav>
+
+<div class="container">
+
+	<header class="big-header text-center">
+		<h1 class="mastoc">Ma Vinothèque</h1>
+		<div class="text-center">
+			<a href="" class="btn btn-default login">Login</a>
+		</div>
+
+	</header>
+
+
+</div>
+
+<script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
